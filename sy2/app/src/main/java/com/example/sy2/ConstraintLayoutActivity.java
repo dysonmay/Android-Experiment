a1c940764ead099cdfd6ca6b3c1bb26fed97c93d
WXJ <2731518003@qq.com>
2025-10-05 16:25:51 +0800

    Commit sy2 Layout

---
+++ sy2/app/src/main/java/com/example/sy2/ConstraintLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.sy2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout);
+    }
+}
